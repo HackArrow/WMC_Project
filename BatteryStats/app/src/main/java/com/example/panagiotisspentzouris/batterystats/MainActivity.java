45848b5904a3aa723037ae2e5439b5ed88c7ed09
Panagiotis Spentzouris <pspentzouris@gmail.com>
2018-01-30 18:42:26 +0200

    Everything except atc

--- BatteryStats/app/src/main/java/com/example/panagiotisspentzouris/batterystats/MainActivity.java
+++ BatteryStats/app/src/main/java/com/example/panagiotisspentzouris/batterystats/MainActivity.java
@@ -1,127 +1,61 @@
 package com.example.panagiotisspentzouris.batterystats;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerView;
-
-import static com.google.android.youtube.player.YouTubePlayer.ErrorReason;
-import static com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
-import static com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener;
-import static com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener;
-import static com.google.android.youtube.player.YouTubePlayer.Provider;
-
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
-import android.widget.TextView;
+
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import android.widget.Toast;
 
 
-public class MainActivity extends YouTubeBaseActivity implements OnInitializedListener {
-    public static final String API_KEY = "AIzaSyDgnLOc7q9r6bjMeWRH-itRtyUFUkrGAPM";
-    //https://www.youtube.com/watch?v=<VIDEO_ID>
-    public static final String VIDEO_ID = "-m3V8w_7vhk";
+public class MainActivity extends Activity implements OnClickListener {
 
-    private RadioGroup radioGroup;
-    private RadioButton good, moderate, bad;
-    private Button button;
-    private TextView textView;
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // attaching layout xml
         setContentView(R.layout.activity_main);
 
-        // Initializing YouTube player view
-        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        FloatingActionButton myFab = findViewById(R.id.fab);
+        myFab.setOnClickListener(this);
 
-        radioGroup = (RadioGroup) findViewById(R.id.myRadioGroup);
+        WebView mWebview = findViewById(R.id.webview);
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+        mWebview.getSettings().setSupportZoom(true);
+        mWebview.getSettings().setBuiltInZoomControls(true);
 
-        radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+        final Activity activity = this;
 
+        mWebview.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // find which radio button is selected
-                if(checkedId == R.id.Good) {
-                    Toast.makeText(getApplicationContext(), "choice: Good",
-                            Toast.LENGTH_SHORT).show();
-                } else if(checkedId == R.id.Moderate) {
-                    Toast.makeText(getApplicationContext(), "choice: Moderate",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "choice: Bad",
-                            Toast.LENGTH_SHORT).show();
-                }
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
             }
-
-        });
-
-        good = (RadioButton) findViewById(R.id.Good);
-        moderate = (RadioButton) findViewById(R.id.Moderate);
-        bad = (RadioButton) findViewById(R.id.Bad);
-        textView = (TextView) findViewById(R.id.text);
-
-        button = (Button)findViewById(R.id.chooseBtn);
-        button.setOnClickListener(new OnClickListener() {
-
+            @TargetApi(android.os.Build.VERSION_CODES.M)
             @Override
-            public void onClick(View v) {
-                int selectedId = radioGroup.getCheckedRadioButtonId();
-
-                // find which radioButton is checked by id
-                if(selectedId == good.getId()) {
-                    textView.setText("Good");
-                } else if(selectedId == moderate.getId()) {
-                    textView.setText("Moderate");
-                } else {
-                    textView.setText("Bad");
-                }
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
             }
         });
+
+        mWebview.loadUrl("http://www.google.com");
+
+        //setContentView(mWebview);
+
     }
 
     @Override
-    public void onInitializationFailure(Provider provider, YouTubeInitializationResult result) {
-        Toast.makeText(this, "Failed to initialize.", Toast.LENGTH_LONG).show();
+    public void onClick(View view) {
+        Intent inent = new Intent(this, YoutubeActivity.class);
+        startActivity(inent);
     }
-
-    @Override
-    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
-        if(null== player) return;
-
-        // Start buffering
-        if (!wasRestored) {
-            //player.cueVideo(VIDEO_ID);
-            player.loadVideo(VIDEO_ID);
-            //player.play();
-        }
-
-        // Add listeners to YouTubePlayer instance
-        player.setPlayerStateChangeListener(new PlayerStateChangeListener() {
-            @Override public void onAdStarted() { }
-            @Override public void onError(ErrorReason arg0) { }
-            @Override public void onLoaded(String arg0) { }
-            @Override public void onLoading() { }
-            @Override public void onVideoEnded() { }
-            @Override public void onVideoStarted() { }
-        });
-
-
-        player.setPlaybackEventListener(new PlaybackEventListener() {
-            @Override public void onBuffering(boolean arg0) { }
-            @Override public void onPaused() { }
-            @Override public void onPlaying() { }
-            @Override public void onSeekTo(int arg0) { }
-            @Override public void onStopped() { }
-        });
-    }
-
 }
